cd4a31b2de565211ea1a9ab88f1fa8711520ae78
bianchengyyds <109009506+bianchengyyds@users.noreply.github.com>
2023-07-29 18:37:57 +0800

    Update stephen.c

--- stephen.c
+++ stephen.c
@@ -58,16 +58,3 @@
 //	return 0;
 //}
 
-#include <stdio.h>
-
-int main()
-{
-	int num1 = 0;
-	int num2 = 0;
-	int sum = 0;
-	printf("请输入两个整数:\n");
-	scanf("%d%d\n", &num1, &num2);
-	sum = num1 + num2;
-	printf("sum=%d\n", sum);
-	return 0;
-}
