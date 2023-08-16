34c331a9346a527745f506d32128d4f5b7ef47ca
bianchengyyds <109009506+bianchengyyds@users.noreply.github.com>
2023-08-16 21:47:57 +0800

    Add files via upload

--- stephen.c
+++ stephen.c
@@ -628,6 +628,350 @@
 //	return 0;
 //}
 
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int age = 0;
+//	printf("请输入你的年龄:\n");
+//	scanf("%d", &age);
+//	if (age >= 18 && age < 60)
+//	{
+//		printf("你已成年!\n");
+//	}
+//	else if (age >= 60)
+//	{
+//		printf("老人家!\n");
+//	}
+//	else
+//	{
+//		printf("小屁孩!\n");
+//	}
+//	return 0;
+//}
 
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if (a == 1)
+//	{
+//		if (b == 2)
+//			printf("haha\n");
+//	}
+//	else
+//	{
+//		printf("666\n");
+//	}
+//	return 0;
+//}
 
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 2;
+//	if (3 == a)
+//	{
+//		printf("666\n");
+//	}
+//	return 0;
+//}
 
+//*11. 判断一个数是奇数还是偶数
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	printf("请输入一个整数:\n");
+//	scanf("%d", &a);
+//	if (0 == a % 2)
+//	{
+//		printf("偶数\n");
+//	}
+//	else
+//	{
+//		printf("奇数\n");
+//	}
+//	return 0;
+//}
+
+//*12. 分别输出1~100之间的奇数和偶数
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	printf("偶数:\n");
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (0 == i % 2)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	printf("\n奇数:\n");
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (0 != i % 2)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int day = 0;
+//	printf("请输入今天星期几:\n");
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case(1):
+//		printf("Monday\n");
+//		break;
+//	case(2):
+//		printf("Tuesday\n");
+//		break;
+//	case 3:
+//		printf("Wednesday\n");
+//		break;
+//	case 4:
+//		printf("星期四\n");
+//		break;
+//	case 5:
+//		printf("星期五\n");
+//		break;
+//	case 6:
+//		printf("星期六\n");
+//		break;
+//	case 7:
+//		printf("星期天\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int day = 6;
+//	switch (day)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("workday\n");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("weekday\n");
+//		break;
+//	default :
+//		printf("输入错误!\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	switch (a)
+//	{
+//	case 1:
+//		a++;
+//	case 2:
+//		b++;
+//	case 3:
+//		switch (a)
+//		{
+//		case 1:
+//			a++;
+//		case 2:
+//			a++;
+//			b++;
+//			break;
+//		}
+//	case 4:
+//		b++;
+//		break;
+//	default :
+//		break;
+//	}
+//	printf("a = %d, b= %d\n",a, b);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	while (i <= 10)
+//	{
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (5 == i)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	/*int i = 1;
+//	while ((i = getchar()) != EOF)
+//	{
+//		putchar(i);
+//	}*/
+//	char password[20] = { 0 };
+//	char ch;
+//	printf("请输入密码:\n");
+//	scanf("%s", password);
+//	printf("请确认Y/N:\n");
+//	while ((ch = getchar()) != '\n')
+//	{
+//		;
+//	}
+//	char a = getchar();
+//	if (a == 'Y')
+//	{
+//		printf("登录成功\n");
+//	}
+//	else
+//	{
+//		printf("登录失败\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//		//初始化 //判断  //调整
+//	for (a = 0; a < 10; a++)
+//	{	 //前闭	 //后开
+//		 //可以表示它的含义:10次循环
+//		if (a = 5)
+//		{
+//			printf("hehe\n");
+//		}
+//		printf("666\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	for (;;)
+//	{
+//		printf("666\n");
+//	}
+//	return 0;
+//}
+
+//*13. 请问下面代码循环多少次？
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	int k = 0;
+//	for (i = 0, k = 0; k = 0; i++, k++)
+//		k++;
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	do
+//	{
+//		if (a == 5)
+//		{
+//			continue;
+//		}
+//		printf("%d ", a);
+//		a++;
+//	} while (a < 10);
+//	return 0;
+//}
+
+//*13.计算 n的阶乘.
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int jc = 1;
+//	printf("请输入一个整数, 要求计算它的阶乘:\n");
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		jc *= i;
+//	}
+//	printf("%d\n", jc);
+//	return 0;
+//}
+
+//*14.计算 1!+ 2!+ 3!+ …… + 10!
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int jc = 1;
+//	int result = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		jc *= i;
+//		result += jc;
+//	}
+//	printf("%d\n", result);
+//	return 0;
+//}
+
+//*15.在一个有序数组中查找具体的某个数字n.(讲解二分查找)
+
+//*16.编写代码, 演示多个字符从两端移动, 向中间汇聚.
+//
+//*17.编写代码实现, 模拟用户登录情景, 并且只能登录三次.(只允许输入三次密码，如果密码正确则
+//提示登录成, 如果三次均输入错误, 则退出程序.)
